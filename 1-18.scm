9a8f72e20b9dee33930eccffbe1a976bbbf447d7
Carlos Agarie <carlos.agarie@gmail.com>
2014-07-30 21:11:59 -0300

    Oooops

--- 1-18.scm
+++ 1-18.scm
@@ -1,15 +1,20 @@
 (define (even? n)
   (= (remainder n 2) 0))
 
-(define (double x) (* x 2))
+(define (double x) (+ x x))
 
 (define (halve x) (/ x 2))
 
-(define (very-fast-mult a b)
-  (cond ((= b 0) a)
-        ((even? b) (very-fast-mult (double a) (halve b)))
-        (else (very-fast-mult (+ a a) (- b 1)))))
+(define (very-fast-mult a b k)
+  (cond ((= b 1) (+ a k))
+        ((even? b) (very-fast-mult (double a) (halve b) k))
+        (else (very-fast-mult a (- b 1) (+ k a)))))
 
+(define (* a b) (very-fast-mult a b 0))
+
+(very-fast-mult 2 4 0)
+(very-fast-mult 2 3 0)
+(very-fast-mult 13 13 0)
+(very-fast-mult 42 42 0)
 (* 2 4)
-(* 13 13)
-(* 42 42)
+(* 123412 18473189)
