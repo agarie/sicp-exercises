1114ae0c026a32e1e1a62fba2cb8452c5458d40d
Carlos Agarie <carlos.agarie@gmail.com>
2014-08-02 17:14:01 -0400

    Moarrr exercises

---
+++ 1-22.scm
@@ -0,0 +1,42 @@
+; Prime testing.
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (square x) (* x x))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+  ((divides? test-divisor n) test-divisor)
+  (else (find-divisor n (+ test-divisor 1)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+; Benchmarking.
+(define (timed-prime-test n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime n (- (runtime) start-time))))
+
+(define (report-prime n elapsed-time)
+  (newline)
+  (display n)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes first last)
+  (define (search-iter cur)
+    (if (<= cur last) (timed-prime-test cur))
+    (if (<= cur last) (search-iter (+ cur 2))))
+  (search-iter (if (even? first) (+ first 1) first)))
+
+; Using bigger numbers -- we are in 2014!
+(search-for-primes 1000000000 1000000099)
+(search-for-primes 10000000000 10000000099)
+(search-for-primes 100000000000 100000000099)
+(search-for-primes 1000000000000 1000000000099)
