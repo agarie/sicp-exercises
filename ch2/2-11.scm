164eafb2e90b2386ea05cf80e4177f24761c0b78
Carlos Agarie <carlos.agarie@gmail.com>
2020-07-11 23:55:53 -0300

    Started working on 2-11

---
+++ ch2/2-11.scm
@@ -0,0 +1,6 @@
+; 2.11
+;
+; In passing, Ben also cryptically comments: "By testing the signs of the
+; end-points of the intervals, it is possible to break `mul-interval` into nine
+; cases, only one of which requires more than two multiplications." Rewrite
+; this procedure using Ben's suggestion.
